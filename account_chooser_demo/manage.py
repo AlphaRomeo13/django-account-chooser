7c86e547857822f4776b304512b74f1492f45612
Mahmoud Yaser <me.MahmoudYaser@gmail.com>
2013-04-14 16:42:17 +0200

    fixing manage at heroku

--- account_chooser_demo/manage.py
+++ account_chooser_demo/manage.py
@@ -7,7 +7,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                      os.path.pardir)))
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "account_chooser_demo.settings")
+    if os.getenv('RUN_ENV') == 'development':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "account_chooser_demo.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                  "account_chooser_demo.account_chooser_demo.heroku_settings")
 
     from django.core.management import execute_from_command_line
 
