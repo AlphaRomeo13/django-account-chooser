5c248d11f9e9b4ea131c14f88303dbda820dcc86
Mahmoud Yaser <me.MahmoudYaser@gmail.com>
2013-04-16 10:40:55 +0200

    modified:   account_chooser_demo/demo/urls.py

--- account_chooser_demo/demo/urls.py
+++ account_chooser_demo/demo/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
-from demo.views import index, facebook_signup, twitter_signup, twitter_auth, gplus_auth
+from demo.views import index, facebook_signup, twitter_signup, twitter_auth
 
 urlpatterns = patterns('account_chooser.views',
-							url(r'index/$', index, name='demo_index'),
-							url(r'facebook/$', facebook_signup, name='demo_facebook'),
-							url(r'twitter/$', twitter_signup, name='demo_twitter'),
-							url(r'twitter_auth/$', twitter_auth, name='demo_twitter'),
+    url(r'index/$', index, name='demo_index'),
+    url(r'facebook/$', facebook_signup, name='demo_facebook'),
+    url(r'twitter/$', twitter_signup, name='demo_twitter'),
+    url(r'twitter_auth/$', twitter_auth, name='demo_twitter'),
 )
